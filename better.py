5df73f2f32a2bee930b98bf8127c814944ee6815
Lucas Biasatti <biasattil@gmail.com>
2024-12-12 23:36:00 -0600

    replace selenium use with opggv2 live game functionality

--- better.py
+++ better.py
@@ -4,6 +4,7 @@ from types import NoneType
 import requests
 import time
 import pickle
+import os
 
 from rich.console import Console
 from rich.markdown import Markdown
@@ -11,20 +12,24 @@ from rich.progress import track
 from rich.theme import Theme
 from rich.table import Table
 
+from dotenv import find_dotenv, load_dotenv
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
+from opgg.v2 import search_result
 from opgg.v2.opgg import OPGG
 from opgg.v2.params import Region
 from opgg.v2.search_result import SearchResult
 from opgg.v2.summoner import Summoner
+from opgg.v2.types.response import LiveGameResponse
+from opgg.v2.season import TierInfo
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 
 #Touch
-key = 'RGAPI-98d5f5ae-36e8-4020-b89c-9a2b9d14079e'
-accountNamesToLookAt = [['Air Coots','Prime'],['SEN TenZ','81619'],['Glenn Danzig SMP','demon'],['Karasmai Kayn','NA1'],['I will trade','NA1']]
+key = os.getenv('RIOT_API_KEY')
+accountNamesToLookAt = [['Air Coots','Prime'],['Glenn Danzig SMP','demon'],['SEN TenZ','81619'],['Karasmai Kayn','NA1'],['I will trade','NA1']]
 region = ''
 
 #Don't touch
@@ -33,10 +38,6 @@ liveGame = []
 
 blueSideWinProb = 0.0
 redSideWinProb = 0.0
-
-chromeOptions = Options()
-chromeOptions.add_argument("--log-level=3")
-chromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 console = Console(highlight=False)
 
@@ -103,107 +104,60 @@ def getData(liveGame):
     
         opgg_obj.update(searchResult[0])
 
-        #Scrape opgg to get info that isn't available in riot api or opgg.py - mainly their role
-        #TEMPORARY until opgg.py adds live game lookup
-        newName = tempName.replace('#','-').replace(' ','%20')
-        url = f'https://www.op.gg/summoners/na/{newName}/ingame'
-        
-        driver = webdriver.Chrome(options=chromeOptions)
-        driver.get(url)
+        live_game: LiveGameResponse = opgg_obj.get_live_game(searchResult[0]).data
 
-        time.sleep(5)
-        
-        row_data = []
-        rank_data = []
-        table_bodys = driver.find_elements(By.TAG_NAME, "tbody")
-
-        for table in table_bodys:
-            table_rows = table.find_elements(By.TAG_NAME, "tr")
-
-            for row in table_rows:
-                table_data = row.find_elements(By.TAG_NAME, "td")
-                for data in table_data:
-                    dataClass = data.get_attribute('class')
-                    if('Level' in data.text and '#' in data.text):
-                        row_data.append(data.text)
-                    if('current-rank' in dataClass):
-                        if('Level' in data.text):
-                            rank_data.append('UNRANKED')
-                        else:
-                            rank_data.append(data.text)
-
-        driver.close()
-
-        finalNames = []
-        for name in row_data:
-            index = name.rfind('Level')
-            finalNames.append(name[:index - 1])
-
-        ranks = []
-        for rank in rank_data:
-            index = rank.find(' ')
-            ranks.append(rank[:index].upper())
-
-        #Start building the data list for live game
         data = list()
 
         #Only the patch # matters, the rest will get          
-        data.append(liveGame['gameId'])
+        data.append(live_game.game_id)
         data.append('14.23')
         data.append('TRUE')
 
-        #Make lists for each player in game, populate into master list
         blueTop, blueJG, blueMid, blueADC, blueSup, redTop, redJG, redMid, redADC, redSup = ([] for i in range(10))
         playerInfo = [blueTop, blueJG, blueMid, blueADC, blueSup, redTop, redJG, redMid, redADC, redSup]
 
-        console.print()
+        for i in track(range(10), description='[bold]Getting stats for players...', style='bold white', complete_style='bold cyan', finished_style='bold cyan', pulse_style='bold cyan'):
+            
+            playerTeam = live_game.participants[i].team_key
+            playerRole = live_game.participants[i].position
 
-        #Iterate through each player in list, populate with info pertaining to them
-        for inx, player in track(enumerate(playerInfo), description='[bold]Getting stats for players...', style='bold white', complete_style='bold cyan', finished_style='bold cyan', pulse_style='bold cyan'):
-            
-            player.append(finalNames[inx])
-            player.append('BLUE') if inx < 5 else player.append('RED')
-            player.append(ranks[inx])
-            player.append(1)
-            if(inx == 0 or inx == 5):
-                player.append('TOP')
-            elif(inx == 1 or inx == 6):
-                player.append('JUNGLE')
-            elif(inx == 2 or inx == 7):
-                player.append('MIDDLE')
-            elif(inx == 3 or inx == 8):
-                player.append('ADC')
-            else:
-                player.append('SUPPORT')
+            player_index_map = {
+                ('BLUE', 'TOP'): 0,
+                ('BLUE', 'JUNGLE'): 1,
+                ('BLUE', 'MID'): 2,
+                ('BLUE', 'ADC'): 3,
+                ('BLUE', 'SUPPORT'): 4,
+                ('RED', 'TOP'): 5,
+                ('RED', 'JUNGLE'): 6,
+                ('RED', 'MID'): 7,
+                ('RED', 'ADC'): 8,
+                ('RED', 'SUPPORT'): 9
+            }
+            playerIndex = player_index_map.get((playerTeam, playerRole), 9)
 
-            for val in liveGame['participants']:
-                if(finalNames[inx] in val['riotId']):
-                    #print(val['championId'])
-                    player.append(int(val['championId']))
-            
-            while True:
-                try:
-                    currentSummoner: list[SearchResult] = opgg_obj.search(finalNames[inx], region=Region.NA, returns='profile')
-                except Exception as e:
-                    print(f'Exception {e} occured')
-                    continue
-                break
-            
+            playerName = live_game.participants[i].summoner.game_name + '#' + live_game.participants[i].summoner.tagline
+            playerInfo[playerIndex].append(playerName)
+            playerInfo[playerIndex].append(playerTeam)
+            #playerInfo[playerIndex].append(live_game.participants[i].tier_info.tier.upper())
+
+            #temp
+            playerInfo[playerIndex].append('SILVER')
+
+            playerInfo[playerIndex].append(1)
+            playerInfo[playerIndex].append(playerRole)
+            playerInfo[playerIndex].append(live_game.participants[i].champion_id)
+
             try:
-                player.append(((currentSummoner[0].summoner.league_stats[0].win) / (currentSummoner[0].summoner.league_stats[0].win + currentSummoner[0].summoner.league_stats[0].lose)) * 100)
-                player.append(currentSummoner[0].summoner.league_stats[0].is_hot_streak)
+                playerInfo[playerIndex].append(((live_game.participants[i].summoner.league_stats[0].win) / (live_game.participants[i].summoner.league_stats[0].win + live_game.participants[i].summoner.league_stats[0].lose)) * 100)
+                playerInfo[playerIndex].append(live_game.participants[i].summoner.league_stats[0].is_hot_streak)
             except TypeError:
-                player.append(50.0)
-                player.append(False)
-            
-            
-            # if(currentSummoner[0].summoner.league_stats[0].win is not NoneType and currentSummoner[0].summoner.league_stats[0].lose is not NoneType):
-            #     player.append(((currentSummoner[0].summoner.league_stats[0].win) / (currentSummoner[0].summoner.league_stats[0].win + currentSummoner[0].summoner.league_stats[0].lose)) * 100)
-            #     player.append(currentSummoner[0].summoner.league_stats[0].is_hot_streak)
-            # else:
-            #     player.append(50.0)
-            #     player.append(False)
-
+                playerInfo[playerIndex].append(50.0)
+                playerInfo[playerIndex].append(False)
+            except IndexError:
+                playerInfo[playerIndex].append(50.0)
+                playerInfo[playerIndex].append(False)
+        
+        for player in playerInfo:
             data.extend(player)
 
         #Write to CSV
@@ -217,8 +171,6 @@ def manipulateData():
         MARKDOWN = '# Manipulating Data'
         md = Markdown(MARKDOWN, style='bold')
         console.print(md)
-
-        #console.print('\n\nManipulating Data', style='bold cyan')
 
         #Manipulation time
         df = pd.read_csv('gameInfo.csv')
@@ -257,17 +209,17 @@ def manipulateData():
 
         #Change rank text to integer represenatation
         pd.set_option("future.no_silent_downcasting", True)
-        df['BlueTopRank'] = df['BlueTopRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['BlueJungleRank'] = df['BlueJungleRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['BlueMidRank'] = df['BlueMidRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['BlueADCRank'] = df['BlueADCRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['BlueSupportRank'] = df['BlueSupportRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['BlueTopRank'] = df['BlueTopRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['BlueJungleRank'] = df['BlueJungleRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['BlueMidRank'] = df['BlueMidRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['BlueADCRank'] = df['BlueADCRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['BlueSupportRank'] = df['BlueSupportRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
 
-        df['RedTopRank'] = df['RedTopRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['RedJungleRank'] = df['RedJungleRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['RedMidRank'] = df['RedMidRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['RedADCRank'] = df['RedADCRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
-        df['RedSupportRank'] = df['RedSupportRank'].replace('UNRANKED', 0).replace('UNRANKE', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['RedTopRank'] = df['RedTopRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['RedJungleRank'] = df['RedJungleRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['RedMidRank'] = df['RedMidRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['RedADCRank'] = df['RedADCRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
+        df['RedSupportRank'] = df['RedSupportRank'].replace('UNRANKED', 0).replace('IRON', 1).replace('BRONZE', 2).replace('SILVER', 3).replace('GOLD', 4).replace('PLATINUM', 5).replace('EMERALD', 6).replace('DIAMOND', 7).replace('MASTER', 8).replace('GRANDMASTER', 9).replace('CHALLENGER', 10)
 
         #Don't need since we already made classifier
         df = df.drop(['GameVersion'], axis=1)
@@ -309,25 +261,6 @@ def manipulateData():
         console.print(md)
 
         return result
-
-# def predictGame(game):
-
-        # with open('classifier.pkl', 'rb') as f:
-        #     clf = pickle.load(f)
-
-        # predict = clf.predict(game)
-        # result = ''
-        # if('True' in predict):
-        #     result = 'True'
-        # else:
-        #     result = 'False'
-        
-        # print('\n\n================================')
-        # print('Blue side predicted to win?')
-        # print(result)
-        # print('================================')
-
-        # return result
 
 if __name__ == '__main__':
     
